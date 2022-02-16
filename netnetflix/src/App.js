2e9199585bb351e796658ef60ee9564ce78159fb
HadeelDaragmeh158 <hadeelmohammad158@gmail.com>
2022-02-16 23:50:51 +0200

    t19

--- netnetflix/src/App.js
+++ netnetflix/src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import Home from './Components/Home/Home';
-import {Route,Routes } from 'react-router-dom';
+import FavLit from './Components/FavList';
+import { Route, Routes } from 'react-router-dom';
+import  Navbar  from './Components/Navbar/Navbar';
 import React from 'react';
 
 function App() {
   return (
-  <><div>
-  
+    <><div>
 
-  <Routes>
-  <Route path='/' element={<Home />} />
- 
-  </Routes>
-  </div>
-  </>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/FavLit' element={<FavLit />} />
+      </Routes>
+    </div>
+    </>
   );
 }
 
